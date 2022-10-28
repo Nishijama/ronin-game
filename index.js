7244aea509db925634a7ecb5c2721ab51697ac42
Nishijama <nathanplociennik@gmail.com>
2022-10-29 00:48:42 +0200

    created Sprite classes and added gravity

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+//handle mobile
+window.addEventListener('resize', () => {
+    if ( window.innerWidth < window.innerHeight) {
+        alert("Flip you phone :)")
+    }
+})
+
+
+//project initialization
+
+
 const canvas = document.getElementById("canvas1")
 
 const c = canvas.getContext('2d')
@@ -5,10 +16,46 @@ const c = canvas.getContext('2d')
 canvas.width = 1024;
 canvas.height = 576;
 
-window.addEventListener('resize', () => {
-    if ( window.width < window.height) {
-        alert("Flip you phone :)")
-    }
-})
+const GRAVITY = 0.2;
 
 c.fillRect(0,0,canvas.width, canvas.height)
+
+//sprite
+
+
+class Sprite {
+    constructor({position, velocity}) {
+        this.position = position;
+        this.velocity = velocity;
+        this.height = 100;
+        this.width = 33;
+    }
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+    update() {
+        this.draw()
+        this.position.y += this.velocity.y;
+
+        if (this.position.y + this.height + this.velocity.y >= canvas.height)
+        this.velocity.y = 0;
+            
+        else this.velocity.y += GRAVITY;
+    }
+}
+
+const player = new Sprite({position: { x: 0, y: 0 }, velocity: {x: 0, y: 0}});
+const enemy = new Sprite({position: { x: canvas.width - 33, y:0 }, velocity: {x: 0, y: 0}});
+
+
+function animate() {
+    c.fillStyle = "black"
+    c.fillRect(0,0,canvas.width,canvas.height)
+    player.update();
+    enemy.update();
+
+    window.requestAnimationFrame(animate)
+}
+
+animate();
