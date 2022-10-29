ed39549568d4f0792599b68f23a1f257cadff602
Nishijama <nathanplociennik@gmail.com>
2022-10-29 16:41:05 +0200

    added player movement

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const c = canvas.getContext('2d')
 canvas.width = 1024;
 canvas.height = 576;
 
-const GRAVITY = 0.2;
+const GRAVITY = 0.6;
 
 c.fillRect(0,0,canvas.width, canvas.height)
 
@@ -37,10 +37,10 @@ class Sprite {
     update() {
         this.draw()
         this.position.y += this.velocity.y;
+        this.position.x += this.velocity.x;
 
         if (this.position.y + this.height + this.velocity.y >= canvas.height)
         this.velocity.y = 0;
-            
         else this.velocity.y += GRAVITY;
     }
 }
@@ -48,6 +48,18 @@ class Sprite {
 const player = new Sprite({position: { x: 0, y: 0 }, velocity: {x: 0, y: 0}});
 const enemy = new Sprite({position: { x: canvas.width - 33, y:0 }, velocity: {x: 0, y: 0}});
 
+let lastKey;
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    w: {
+        pressed: false
+    }
+} 
 
 function animate() {
     c.fillStyle = "black"
@@ -55,7 +67,36 @@ function animate() {
     player.update();
     enemy.update();
 
+    player.velocity.x = 0;
+    
+    if (keys.d.pressed && lastKey === 'd') {
+        player.velocity.x = 5;
+    } else if (keys.a.pressed && lastKey === 'a') {
+        player.velocity.x = -5;
+    }
+
     window.requestAnimationFrame(animate)
 }
 
 animate();
+
+window.addEventListener('keydown', (e) => {
+    console.log(e.key)
+    switch(e.key) {
+        case "a":
+            keys.a.pressed = true;
+            lastKey = 'a';
+            break;
+        case "d": 
+            keys.d.pressed = true;
+            lastKey = 'd';
+            break;
+        case "w": 
+            player.velocity.y = -15;
+            break;
+    }
+})
+
+window.addEventListener('keyup', ({key}) => {
+    keys[key].pressed = false;
+})
